aa004f801168af40f357c9a621704ee2cd65fefe
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-08 13:29:44 -0700

    display iceCreamArray in ul tag in index.html
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  const iceCreamArray = ["Key Lime", "Birthday Cake", "Chocolate", "Strawberry Cheesecake", "Cookie Dough"];
+
+  $("#li1").text(iceCreamArray[0]);
+  $("#li2").text(iceCreamArray[1]);
+  $("#li3").text(iceCreamArray[2]);
+  $("#li4").text(iceCreamArray[3]);
+  $("#li5").text(iceCreamArray[4]);
+});
